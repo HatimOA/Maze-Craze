89db420784dba61bd2da8a9141c9f84bc436d3b8
HatimOA <hatimarifi@gmail.com>
2026-06-05 07:41:59 +0100

    Final project improvements in Maze Craze game

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient({
   log: ["error", "warn"],
 });
-=======
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
->>>>>>> e2d8c13 (Deplyoment without docker)
 
 module.exports = prisma;
